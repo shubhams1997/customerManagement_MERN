310f20c73ec21634248e2ca0d6bfff5a54615114
shubhamsharma199782 <shubham.sharma199782@gmail.com>
2020-09-13 00:44:52 +0530

    update other user throught Admin only

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,20 @@ exports.getUserById = (req, res, next, id) => {
         error: "No user found!",
       });
     }
+    console.log("user profile added");
     req.profile = user;
+    next();
+  });
+};
+
+exports.getUserForUpdate = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Invalid Request for user update",
+      });
+    }
+    req.updateProfile = user;
     next();
   });
 };
@@ -17,9 +30,23 @@ exports.getUser = (req, res) => {
   req.profile.encry_password = undefined;
   req.profile.createAt = undefined;
   req.profile.updatedAt = undefined;
-  return res.json(res.profile);
+  return res.json(req.profile);
 };
 
 exports.updateUser = (req, res) => {
-  return res.send("update route");
+  User.findByIdAndUpdate(
+    { _id: req.updateProfile._id },
+    { $set: req.body },
+    { new: true, userFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Failed to update user in DB",
+        });
+      }
+      req.profile.salt = undefined;
+      req.profile.encry_password = undefined;
+      return res.json(user);
+    }
+  );
 };
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, updateUser } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  getUserForUpdate,
+} = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 // parameters
 router.param("userId", getUserById);
+router.param("updateUserId", getUserForUpdate);
 
 // routers
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
-router.put("/user/:userId", isSignedIn, isAuthenticated, isAdmin, updateUser);
+router.put(
+  "/user/:userId/:updateUserId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateUser
+);
 
 module.exports = router;
